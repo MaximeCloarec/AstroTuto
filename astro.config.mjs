28254d67aca366d37f8eef4d9c66c01bfd4d1b08
MaximeCloarec <156484577+MaximeCloarec@users.noreply.github.com>
2025-06-14 20:06:47 +0200

    Finished

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://loquacious-dieffenbachia-d3e847.netlify.app/",
+  site: "https://loquacious-dieffenbachia-d3e847.netlify.app/",
+  integrations: [preact()],
 });
